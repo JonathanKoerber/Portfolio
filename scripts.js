94045602b8a6c053516608d8a01676d0fdbd4ff5
Jonathan <jonathankoerber@gmail.com>
2020-06-19 07:30:24 -0700

    adding scroll actions

--- scripts.js
+++ scripts.js
@@ -3,3 +3,20 @@
 function back(){
   window.history.back();
 }
+// top btn
+topBtn = document.getElementById("topBtn");
+
+window.onscroll = function () {scrollFunction()};
+
+function scrollFunction(){
+  if(document.body.schoolTop > 200 || document.documentElement.scrollTop > 200){
+    topBtn.style.display = "block";
+  }else{
+    topBtn.style.display = "none";
+  }
+}
+
+function toFunction(){
+  document.body.scrollTop = 0; // for Safari
+  document.documentElement.scrollTop = 0; //for Chrome, FF, IE, and Opera
+}
